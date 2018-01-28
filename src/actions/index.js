0e22273cefc409e7e0980c62d5888e6b61d1004e
Adam Arling <aarling@gmail.com>
2018-01-28 13:52:32 -0600

    End of section 5

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '3853f693a179e8e9baf76720851812d6';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   }
 }
